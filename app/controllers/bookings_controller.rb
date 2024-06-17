e278396bf697038675dffc70ea7890bc483a9044
felixDev22 <felixnzomo@gmail.com>
2024-06-17 23:52:36 +0300

    Create the booking methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      render json: @booking, status: :created
+    else
+      render json: @booking.errors, status: :unprocessable_entity 
+    end
+  end
+
+  def index
+    @bookings = Booking.all
+    render json: @bookings
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    render json: @bookings
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :date_of_arrival, :date_of_departure, :adults, :children, :room, :total_price)
+  end
 end
